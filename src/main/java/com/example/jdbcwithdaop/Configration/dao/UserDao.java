09884e572868b3526971ff6d567c88f7d34240b5
kieukhiem <khiem.kieu@ncsgroup.vn>
2023-03-13 23:48:20 +0700

    Revert "first commit"
    This reverts commit 59f00a85148641cdedc97915c55fe7161e485e76.

--- src/main/java/com/example/jdbcwithdaop/Configration/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.jdbcwithdaop.Configration.dao;
-
-import com.example.jdbcwithdaop.Configration.dao.base.Dao;
-import com.example.jdbcwithdaop.Configration.model.User;
-
-public interface UserDao extends Dao<User> {
-}
