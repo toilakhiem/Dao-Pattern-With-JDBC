d208ffc88b6e2ba807c2f4b1527bf7ccd61a4745
kieukhiem <khiem.kieu@ncsgroup.vn>
2023-03-20 19:34:15 +0700

    using crud with jdbc, validate input, add exception

--- src/main/java/com/example/jdbc_with_dao_pattern/exception/NotFoundException.java
+++ src/main/java/com/example/jdbc_with_dao_pattern/exception/NotFoundException.java
@@ -1,11 +1,11 @@
 package com.example.jdbc_with_dao_pattern.exception;
 
-import static com.example.jdbc_with_dao_pattern.constant.JdbcWithDaoPatternConstant.ExceptionRespoinse.CONFLICT_EXCEPTION_CODE;
-import static com.example.jdbc_with_dao_pattern.constant.JdbcWithDaoPatternConstant.ExceptionRespoinse.NOT_FOUND_EXCEPTION;
+
+import static com.example.jdbc_with_dao_pattern.constant.JdbcWithDaoPatternConstant.ExceptionRespoinse.NOT_FOUND_EXCEPTION_CODE;
 
 public class NotFoundException extends BaseException{
     public NotFoundException() {
         setStatus(404);
-        setCode(NOT_FOUND_EXCEPTION);
+        setCode(NOT_FOUND_EXCEPTION_CODE);
     }
 }
